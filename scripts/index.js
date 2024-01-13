0a0747f874a824dc0ba82beb60f50332fb70c7c4
DAGAMES386 <116123691+DAGAMES386@users.noreply.github.com>
2024-01-13 13:40:33 -0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ const canvasC = document.getElementById('c');
 const btn = document.getElementById('ilysm');
 
 const config = {
-  birthdate: 'Jan 12, 2025',
+  birthdate: 'Jan 12, 2024',
   name: 'Pops'
 };
 
